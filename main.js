907105ed404aa0830e7245997e50f03702025db3
Maira Quintana <you@example.com>
2024-05-08 18:07:35 -0300

    refactorizacion de variables - estructura de datos Array

--- main.js
+++ main.js
@@ -15,18 +15,37 @@ let imgFruta1 = 'http://source.unsplash.com/150x150?apple';
 let imgFruta2 = 'http://source.unsplash.com/150x150?pear';
 let imgFruta3 = 'http://source.unsplash.com/150x150?banana';
 
-fruta1.innerHTML = "<img src=" + imgFruta1 +">" +
-                   "<h2>" + nombrefruta1 + "</h2>" + "<p>" + precioFruta1 + "</p>" + 
-                   "<p>" + stockFruta1 +"</p>";
-fruta2.innerHTML = `<img src=${imgFruta2}/>
-                    <h2>${nombrefruta2}</h2>
-                    <h2>${precioFruta2}</h2>
-                    <h2>${stockFruta2}</h2>                   
+let array = [[nombrefruta1, precioFruta1, stockFruta1, imgFruta1],
+             [nombrefruta2, precioFruta2, stockFruta2, imgFruta2],
+             [nombrefruta3, precioFruta3, stockFruta3, imgFruta3] ]
+
+//array[0] = "Sandia"//
+alert(array[0].length)
+
+fruta1.innerHTML =
+    "<img src=" + 
+    array[0][3] +
+    ">" +
+    "<h2>" + 
+    array[0][0]+
+    "</h2>" + 
+    "<p>Precio: " +
+     array[0][1]
+    "</p>" + 
+    "<p>Stock: " + 
+    array[0][2] +
+    "</p>";
+fruta2.innerHTML =
+                   `<img src=${array[1][array-length-1]}/>
+                    <h2>${array[1][0]} </h2>
+                    <h2>${array[1][1]}</h2>
+                    <h2>${array[1][2]}</h2>                   
                     `
-fruta3.innerHTML = `<img src=${imgFruta3}/>
-                    <h2>${nombrefruta3}</h2>
-                    <h2>${precioFruta3}</h2>
-                    <h2>${stockFruta3}</h2>
+fruta3.innerHTML =
+                     `<img src=${array[2][3]}/>
+                    <h2>${array[2][0]}</h2>
+                    <h2>${array[2][1]}</h2>
+                    <h2>${array[2][2]}</h2>
                     
                     `                    
 
